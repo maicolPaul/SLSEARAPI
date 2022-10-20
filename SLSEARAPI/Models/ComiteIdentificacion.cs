9a1235244b12ff1ec0db0c34aa7ca56073c04c94
KnarfSajorN <frank.rojas.joaquin@gmail.com>
2022-10-20 01:57:18 -0500

    frj

---
+++ SLSEARAPI/Models/ComiteIdentificacion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLSEARAPI.Models
+{
+    public class ComiteIdentificacion
+    {
+        public int piPageSize { get; set; }
+        public int piCurrentPage { get; set; }
+        public string pvSortColumn { get; set; }
+        public string pvSortOrder { get; set; }
+        public int iCodComiteIdentificacion { get; set; }
+        public int iCodComiteEvaluador { get; set; }
+        public int iCodIdentificacion { get; set; }
+        public string dFechaRegistro { get; set; }
+        public bool bActivo { get; set; }
+        public string vNombreSearT1 { get; set; }
+        public string vDireccionT2 { get; set; }
+        public string iCodUbigeoT1 { get; set; }
+        public string vNomDepartamento { get; set; }
+        public string vNomProvincia { get; set; }
+        public string vNomDistrito { get; set; }
+        public int iRecordCount { get; set; }
+
+    }
+}
