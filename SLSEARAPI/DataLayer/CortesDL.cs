8ce9b30025a0d101801e6a4cf195563404e1722c
MICHAEL PAUL AYLAS SOTO <desarrollador214_usi@minedu.gob.pe>
2022-11-18 20:55:10 -0500

    .

---
+++ SLSEARAPI/DataLayer/CortesDL.cs
@@ -0,0 +1,131 @@
+﻿using SLSEARAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SLSEARAPI.DataLayer
+{
+    public class CortesDL
+    {
+        public CortesCabecera InsertarCorteCabecera(CortesCabecera cortesCabecera)
+        {
+			try
+			{
+                using (SqlConnection conection = new SqlConnection(ConfigurationManager.ConnectionStrings["cnx"].ConnectionString))
+                {
+                    conection.Open();
+
+                    using (SqlCommand command = new SqlCommand("[PA_InsertarCortesCabecera]", conection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@iCodFichaTecnica", cortesCabecera.iCodFichaTecnica);
+                        command.Parameters.AddWithValue("@dFechaInicioReal", cortesCabecera.dFechaInicioReal);
+                        command.Parameters.AddWithValue("@dFechaFinReal", cortesCabecera.dFechaFinReal);
+
+                        using (SqlDataReader dr = command.ExecuteReader())
+                        {
+                            if (dr.HasRows)
+                            {
+
+                                if (dr.Read())
+                                {
+                                    cortesCabecera.iCodCorte = dr.GetInt32(dr.GetOrdinal("iCodCorte"));
+                                    cortesCabecera.vMensaje = dr.GetString(dr.GetOrdinal("vMensaje"));
+                                }
+                            }
+                        }
+                    }
+                    conection.Close();
+                }
+            }
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+
+            return cortesCabecera;
+        }
+        public CortesDetalle InsertarCorteDetalle(CortesDetalle cortesDetalle)
+        {
+            try
+            {
+                using (SqlConnection conection = new SqlConnection(ConfigurationManager.ConnectionStrings["cnx"].ConnectionString))
+                {
+                    conection.Open();
+
+                    using (SqlCommand command = new SqlCommand("[PA_InsertarCortesDetalle]", conection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@iCodCorte", cortesDetalle.iCodCorte);
+                        command.Parameters.AddWithValue("@idias", cortesDetalle.idias);
+                       
+                        using (SqlDataReader dr = command.ExecuteReader())
+                        {
+                            if (dr.HasRows)
+                            {
+
+                                if (dr.Read())
+                                {
+                                    cortesDetalle.iCodCorte = dr.GetInt32(dr.GetOrdinal("iCodCorteDetalle"));
+                                    cortesDetalle.vMensaje = dr.GetString(dr.GetOrdinal("vMensaje"));
+                                }
+                            }
+                        }
+                    }
+                    conection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return cortesDetalle;
+        }
+        public List<CortesDetalle> ListarCortesDetalle(CortesDetalle cortesDetalle)
+        {
+            List<CortesDetalle> lista = new List<CortesDetalle>();
+
+            try
+            {
+                using (SqlConnection conection = new SqlConnection(ConfigurationManager.ConnectionStrings["cnx"].ConnectionString))
+                {
+                    conection.Open();
+
+                    using (SqlCommand command = new SqlCommand("[PA_ListarCortesDetalle]", conection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@iCodCorte", cortesDetalle.iCodCorte);
+                        
+                        using (SqlDataReader dr = command.ExecuteReader())
+                        {
+                            if (dr.HasRows)
+                            {
+                                while(dr.Read())
+                                {
+                                    cortesDetalle = new CortesDetalle();
+                                    cortesDetalle.iCodCorteDetalle = dr.GetInt32(dr.GetOrdinal("iCodCorteDetalle"));
+                                    cortesDetalle.iCodCorte = dr.GetInt32(dr.GetOrdinal("iCodCorte"));
+                                    cortesDetalle.idias = dr.GetInt32(dr.GetOrdinal("idias"));
+                                    cortesDetalle.vMensaje = dr.GetString(dr.GetOrdinal("vMensaje"));
+                                    lista.Add(cortesDetalle);
+                                }
+                            }
+                        }
+                    }
+                    conection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ SLSEARAPI/Models/CortesDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLSEARAPI.Models
+{
+    public class CortesDetalle
+    {
+        public int iCodCorteDetalle { get; set; }
+
+        public int iCodCorte { get; set; }
+
+        public int idias { get; set; }
+
+        public string vMensaje { get; set; }
+
+
+    }
+}
