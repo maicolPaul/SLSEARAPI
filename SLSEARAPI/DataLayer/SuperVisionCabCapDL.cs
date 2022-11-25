7c6c1221b790c0b5db1b95a1391cb9a45e83f618
MICHAEL PAUL AYLAS SOTO <desarrollador214_usi@minedu.gob.pe>
2022-11-25 09:06:04 -0500

    .

---
+++ SLSEARAPI/DataLayer/SuperVisionCabCapDL.cs
@@ -0,0 +1,61 @@
+﻿using SLSEARAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SLSEARAPI.DataLayer
+{
+    public class SuperVisionCabCapDL
+    {
+        public SupervisionCapCab InsertarSuperVisionCabCap(SupervisionCapCab entidad)
+        {
+            try
+            {
+                using (SqlConnection conection = new SqlConnection(ConfigurationManager.ConnectionStrings["cnx"].ConnectionString))
+                {
+                    conection.Open();
+
+
+                    using (SqlCommand command = new SqlCommand("[TB_Supervisio_CapCab]", conection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@iCodIdentificacion", entidad.iCodComponente);
+                        command.Parameters.AddWithValue("@iCodFichaTecnica", entidad.iCodFichaTecnica);
+                        command.Parameters.AddWithValue("@iCodComponente", entidad.iCodComponente);
+                        command.Parameters.AddWithValue("@iCodActividad", entidad.iCodActividad);
+                        command.Parameters.AddWithValue("@vObservaciongeneral", entidad.vObservaciongeneral);
+                        command.Parameters.AddWithValue("@vRecomendacion", entidad.vRecomendacion);
+
+
+
+                        using (SqlDataReader dr = command.ExecuteReader())
+                        {
+                            if (dr.HasRows)
+                            {
+                                if (dr.Read())
+                                {
+
+                                    entidad.iCodSuperCab = dr.GetInt32(dr.GetOrdinal("iCodHitos"));
+
+                                    entidad.vMensaje = dr.GetString(dr.GetOrdinal("vMensaje"));
+                                }
+                            }
+                        }
+
+                    }
+                    conection.Close();
+                }
+                return entidad;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ SLSEARAPI/Models/SupervisionCapCab.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLSEARAPI.Models
+{
+    public class SupervisionCapCab
+    {
+        public int iCodSuperCab { get; set; }
+
+        public int iCodIdentificacion { get; set; }
+
+        public int iCodFichaTecnica { get; set; }
+
+        public int iCodComponente { get; set; }
+
+        public int iCodActividad { get; set; }
+
+        public string vObservaciongeneral { get; set; }
+
+        public string vRecomendacion { get; set; }
+
+        public string vNombreSupervisor { get; set; }
+
+        public string vCargoSupervisor { get; set; }
+
+        public string vEntidadSupervisor { get; set; }
+
+        public int iCodCalificacion { get; set; }
+
+        public string vMensaje { get; set; }
+
+    }
+}
